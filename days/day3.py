0a221d049315a7e05bc6159bc6aa7aa73c1114ff
Conor Walsh <conor@foo.solutions>
2025-07-21 00:37:12 -0400

    day3 prep

---
+++ days/day3.py
@@ -0,0 +1,17 @@
+import os
+import bisect
+
+def part1():
+    safe_reports = 0
+    with open("./input/2.txt") as f:
+        for line in f:
+            vals = list(map(int, line.split(" ")))
+    return safe_reports
+
+
+def part2():
+    safe_reports = 0
+    with open("./input/2.txt") as f:
+        for line in f:
+            vals = list(map(int, line.split(" ")))
+    return safe_reports
