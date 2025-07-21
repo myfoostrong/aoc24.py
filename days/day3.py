fc46928704ff5d443332dea91123724a84d7f372
Conor Walsh <conor@foo.solutions>
2025-07-21 09:14:04 -0400

    solved day2.1

--- days/day3.py
+++ days/day3.py
@@ -1,17 +1,19 @@
 import os
-import bisect
+import re
 
 def part1():
     safe_reports = 0
-    with open("./input/2.txt") as f:
+    with open("./input/3.txt") as f:
         for line in f:
-            vals = list(map(int, line.split(" ")))
+            x = re.findall("mul\(([0-9]*),([0-9]*)\)", line)
+            for pair in x:
+                safe_reports += (int(pair[0]) * int(pair[1]))
     return safe_reports
 
 
 def part2():
     safe_reports = 0
-    with open("./input/2.txt") as f:
+    with open("./input/3.txt") as f:
         for line in f:
-            vals = list(map(int, line.split(" ")))
+            x = re.findall("mul\(([0-9]*),([0-9]*)\)", line)
     return safe_reports
